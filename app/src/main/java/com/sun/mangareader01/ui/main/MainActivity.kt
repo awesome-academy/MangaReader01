9a8909ae68401e44c0e86ac86ad6a08dd7a5fd49
PhucSSK <lhphuc.ynwa@gmail.com>
2019-06-08 10:56:05 +0700

    Handle Onclick Search Button

--- app/src/main/java/com/sun/mangareader01/ui/main/MainActivity.kt
+++ app/src/main/java/com/sun/mangareader01/ui/main/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.sun.mangareader01.ui.main
 
 import android.os.Bundle
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import com.sun.mangareader01.R
 import com.sun.mangareader01.ui.home.HomeFragment
 import com.sun.mangareader01.ui.mycomics.MyComicsFragment
+import com.sun.mangareader01.ui.search.SearchFragment
 import com.sun.mangareader01.ui.trending.TrendingFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -32,15 +34,34 @@ class MainActivity : FragmentActivity() {
             }?.let { replaceFragment(it) }
             true
         }
-        viewSearch.setOnClickListener {
-            viewSearch.isIconified = false
+        viewSearch.apply {
+            setOnClickListener { isIconified = false }
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    addFragment(SearchFragment.newInstance(query))
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    // Quick search response here
+                    return true
+                }
+
+            })
         }
+    }
+
+    private fun addFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().add(
+            R.id.layoutContainerFragment,
+            fragment
+        ).addToBackStack(null).commit()
     }
 
     private fun replaceFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(
             R.id.layoutContainerFragment,
             fragment
-        ).commit()
+        ).addToBackStack(null).commit()
     }
 }
